37a59e6285ad2b32f8918454a63ac6e1d09280fc
zyerin <zyerin5974@gmail.com>
2025-05-05 19:07:44 +0900

    feat: 계정 연동 엔티티 생성

---
+++ src/main/java/com/example/iplan/Domain/PendingAccountRequest.java
@@ -0,0 +1,22 @@
+package com.example.iplan.Domain;
+
+import com.google.cloud.firestore.annotation.DocumentId;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(description = "계정 연동 요청 엔티티")
+public class PendingAccountRequest {
+    @DocumentId
+    private String id;
+
+    private String childNickname;
+    private String parentNickname;
+    private boolean approved; // 자녀가 승인했는지 여부
+}
