522bb818d38407a0590882d5106a1819582c81ad
zyerin <zyerin5974@gmail.com>
2025-05-13 23:08:48 +0900

    feat: 계정 연동 관련 DTO 생성

---
+++ src/main/java/com/example/iplan/DTO/AccountRequestDTO.java
@@ -0,0 +1,18 @@
+package com.example.iplan.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(description = "계정 연동 요청 DTO")
+public class AccountRequestDTO {
+    private String childNickname;
+    private String parentNickname;
+    private boolean approved; // 자녀가 승인했는지 여부
+}
