c328ebcc1b5c36121062f692a3f01074a1ce3ceb
zyerin <zyerin5974@gmail.com>
2025-06-21 16:48:02 +0900

    feat: 부모 피드백 엔티티 생성

---
+++ src/main/java/com/example/iplan/Domain/Feedback.java
@@ -0,0 +1,35 @@
+package com.example.iplan.Domain;
+
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.firebase.database.annotations.NotNull;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Feedback {
+
+    @DocumentId
+    private String id; // Firestore 문서의 ID (지급한 보상의 id)
+
+    @NotNull
+    private String user_id; // 부모님의 닉네임
+
+    @NotNull
+    private String child_id;  // 아이의 닉네임
+
+    @NotNull
+    private String reward_id;   // 아이들이 작성한 보상과 맵핑
+
+    private String comment; // 부모님의 코멘트
+
+    private int grade; // 부모님의 별점
+
+    private boolean rewarded; // 보상이 지급되었는지 여부 -> 첨삭 여부
+
+    private boolean success; // 보상이 지급 or 보류
+}
