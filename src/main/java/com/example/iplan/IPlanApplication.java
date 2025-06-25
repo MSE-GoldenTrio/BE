a30ff283e43ddb36164f316ac03bcc1c31b34e98
zyerin <zyerin5974@gmail.com>
2025-06-26 01:05:55 +0900

    feat: JwtProperties 설정 클래스 활성화를 위한 애플리케이션 설정 추가
    - @EnableConfigurationProperties(JwtProperties.class) 추가하여 JWT 설정 프로퍼티 바인딩 가능하도록 설정
    - JwtProperties 클래스의 필드를 application.yml에서 자동 주입받을 수 있도록 구성

--- src/main/java/com/example/iplan/IPlanApplication.java
+++ src/main/java/com/example/iplan/IPlanApplication.java
@@ -1,12 +1,15 @@
 package com.example.iplan;
 
+import com.example.iplan.auth.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableConfigurationProperties(JwtProperties.class)
 public class IPlanApplication {
 
 	public static void main(String[] args) {
