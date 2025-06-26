237aab15c18d8483d355fb879d154349ac18ad38
zyerin <zyerin5974@gmail.com>
2025-06-26 11:03:08 +0900

    refact: 불필요한 existsById 메서드 제거

--- src/main/java/com/example/iplan/auth/redis/BlacklistRepository.java
+++ src/main/java/com/example/iplan/auth/redis/BlacklistRepository.java
@@ -3,5 +3,4 @@ package com.example.iplan.auth.redis;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BlacklistRepository extends CrudRepository<Blacklist, String> {
-    boolean existsById(String id); // id = nickname
 }
