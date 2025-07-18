26fd00dcf896c733e3c0a080a7717f9c472b0be4
zyerin <zyerin5974@gmail.com>
2025-07-18 12:23:32 +0900

    docs: FcmRequestDTO에서 pendingRequestId의 쓰임 로그 추가!!

--- src/main/java/com/example/iplan/fcm/FcmRequestDTO.java
+++ src/main/java/com/example/iplan/fcm/FcmRequestDTO.java
@@ -32,7 +32,15 @@ public class FcmRequestDTO {
     @NoArgsConstructor
     @Builder
     public static class Data {
-        private String pendingRequestId;    // 계정 연동 요청 시에 데이터 담아서 보내야됨 (계획 알림 시에는 null로)
+
+        private String pendingRequestId;    //계획 알림 시에는 null
+        // pendingRequestId의 쓰임
+        // 1. 계정 연동 '요청' 시에는 아이가 해당 요청(PendingAccountRequest)의 Id를 받아야 함!!
+        // -> 승인/거부 수락 시 AccountRequestDTO 에 id 값을 넣어서 보내야하므로
+
+        // 2. 계정 연동 ''해제' 시에도 부모가 해제하면 아이에게 해당 해제 요청 Id(PendingAccountRequest)를 보냄
+        // -> 아이가 이를 확인하면 PendingAccountRequest 에서 id 값에 따라 부모, 아이 닉네임을 기반으로 토큰 재발급 가능(계정 연동 요청 로직과 같음)
+
         private String sender;
         private String type;
     }
