b64cf69e858e2b07fdf63d5f2d94a80916f4ca20
zyerin <zyerin5974@gmail.com>
2025-07-18 09:39:31 +0900

    feat: 부모가 아이에게 연동요청을 보낼 때 Data를 함께 보내야하므로 FcmRequestDTO에 추가

--- src/main/java/com/example/iplan/fcm/FcmRequestDTO.java
+++ src/main/java/com/example/iplan/fcm/FcmRequestDTO.java
@@ -10,9 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class FcmRequestDTO {
+
+    // 푸시알림을 받을 유저 닉네임(프론트에서 쓰이는게 아니라 토큰이 더 이상 유효하지 않음 → DB 에서 삭제할 때 쓰임)
     private String user_id;
+
     private String fcmToken;
     private Notification notification;
+    private Data data;
 
     @lombok.Data
     @AllArgsConstructor
@@ -21,5 +25,15 @@ public class FcmRequestDTO {
     public static class Notification {
         private String title;  // 알림 제목
         private String body;   // 알림 본문
+    }
+
+    @lombok.Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Data {
+        private String pendingRequestId;    // 계정 연동 요청 시에 데이터 담아서 보내야됨 (계획 알림 시에는 null로)
+        private String sender;
+        private String type;
     }
 }
