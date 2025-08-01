f152cf901623c0c7706b3f6353c2511e2eae1e41
zyerin <zyerin5974@gmail.com>
2025-08-01 21:21:33 +0900

    refact: [AccountParentController] 불필요한 메서드 삭제 및 파라미터 명확화
    - 연동 요청이 거부된 것은 부모가 확인 후에 PendingAccountRequest에서 삭제하는 api 요청 제거
    - why? 클라이언트 측에서 onSnapshot 감지로 연동 요청 승인/거부 여부를 확인하고 해당 문서 삭제까지 진행함. 즉, 서버로의 요청이 불필요해짐

--- src/main/java/com/example/iplan/Controller/AccountParentController.java
+++ src/main/java/com/example/iplan/Controller/AccountParentController.java
@@ -29,33 +29,25 @@ public class AccountParentController {
     private final UserRepository userRepository;
 
     // 부모가 아이에게 연동 요청 보냄
+    // DTO에서 childNickname만 받아옴!!
     @Operation(summary = "부모가 아이 계정 연동 요청을 보냄 POST", description = "해당 요청을 PendingAccountRequest 저장")
     @PostMapping("/link-child")
     public ResponseEntity<Map<String, Object>> parentAccountRequest(@RequestBody @NotNull AccountRequestDTO requestDTO, @AuthenticationPrincipal CustomOAuth2UserDetails user)
             throws ExecutionException, InterruptedException {
-        log.info("Account API from parent received!");
+        log.info("부모의 계정 연동 요청 도착");
 
         String childNickname = requestDTO.getChildNickname();
-        String encryptedParentNickname = user.getUsername();
+        String parentEncryptedNickname = user.getUsername();
 
-        return accountParentService.sendAccountRequest(childNickname, encryptedParentNickname);
+        return accountParentService.sendAccountRequest(childNickname, parentEncryptedNickname);
     }
 
     // 부모가 이미 보낸 연동요청이 있는지 체크
     @GetMapping("/check-pending-status")
     public ResponseEntity<Map<String, Object>> checkPendingStatus(@AuthenticationPrincipal CustomOAuth2UserDetails user) {
-        log.info("Check pending status API from parent received");
-        String encryptedParentNickname = user.getUsername();
-        return accountParentService.getParentPendingStatus(encryptedParentNickname);
-    }
-
-    // 연동 요청이 거부된 것은 부모가 확인 후에 엔티티에서 삭제
-    @DeleteMapping("/delete-denied-request")
-    public ResponseEntity<Map<String, Object>> deleteDeniedRequest(
-            @AuthenticationPrincipal CustomOAuth2UserDetails user) {
-        log.info("Delete denied request API from parent received");
-
-        return accountParentService.deleteDeniedRequest(user.getUsername());
+        log.info("부모가 이미 보낸 연동요청이 있는지 체크");
+        String parentEncryptedNickname = user.getUsername();
+        return accountParentService.getParentPendingStatus(parentEncryptedNickname);
     }
 
 }
