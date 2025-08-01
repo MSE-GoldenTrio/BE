aa4259efc796fc8b1102407a5d7a4638456e88e2
zyerin <zyerin5974@gmail.com>
2025-08-01 21:37:14 +0900

    feat: [AccountChildService] 아이에게 누락된 연동 요청 푸시알림 (FCM) 다시 보내는 서비스 로직 작성 + childEncryptedNickname 변경 반영
    - PendingAccountRequest의 childHashedNickname -> childEncryptedNickname 변경으로 인한 로직 수정
    - 아이가 부모의 요청을 승인/거부하는 과정에서 PendingAccountRequest의 유저와의 비교 과정에서 기존에 해시값으로 비교하던 것을 암호화된 아이의 닉네임과 비교하도록 수정

--- src/main/java/com/example/iplan/Service/AccountChildService.java
+++ src/main/java/com/example/iplan/Service/AccountChildService.java
@@ -6,19 +6,21 @@ import com.example.iplan.ExceptionHandler.CustomException;
 import com.example.iplan.Repository.AccountRepository;
 import com.example.iplan.auth.UserRepository;
 import com.example.iplan.auth.Users;
-import com.example.iplan.auth.oauth2.CustomOAuth2UserDetails;
-import com.example.iplan.auth.jwt.JwtToken;
-import com.example.iplan.auth.jwt.JwtTokenProvider;
+import com.example.iplan.fcm.FcmRequestDTO;
+import com.example.iplan.fcm.FcmRequestService;
+import com.example.iplan.fcm.FcmToken;
+import com.example.iplan.fcm.FcmTokenService;
 import com.example.iplan.util.AES256Encryptor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -28,41 +30,43 @@ public class AccountChildService {
 
     private final AccountRepository accountRepository;
     private final UserRepository userRepository;
-    private final JwtTokenProvider jwtTokenProvider;
     private final AES256Encryptor aes;
+    private final FcmRequestService fcmRequestService;
+    private final FcmTokenService fcmTokenService;
 
     /**
      * 아이가 부모의 요청을 승인 or 거부
      * 이때 아이의 linked_id에 이미 부모가 존재한다면 수락 못하도록 !!
      */
-    public Users respondToRequest(String encryptedChildNickname, AccountRequestDTO dto)
+    public Users respondToRequest(String childEncryptedNickname, AccountRequestDTO dto)
             throws Exception {
 
-        log.info("연동 요청 응답 서비스");
+        log.info("아이의 연동 요청 응답 서비스");
 
-        // 1. 요청 ID로 PendingAccountRequst 조회 (부모가 요청 보낼 때 저장되어있음)
+        // 1. DTO에서 받아온 요청 ID로 PendingAccountRequest 조회 (부모가 요청 보낼 때 PendingAccountRequest 저장)
         PendingAccountRequest request = accountRepository.findEntityByDocumentId(dto.getId());
         if (request == null) {
             throw new CustomException("해당 요청이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
         }
 
-        // 해시값으로 아이 닉네임 비교
-        if (!request.getChildHashedNickname().equals(DigestUtils.sha256Hex(aes.decrypt(encryptedChildNickname)))) {
+        // 2. (암호화된) 아이 닉네임 비교
+        if (!request.getChildEncryptedNickname().equals(childEncryptedNickname)) {
             throw new CustomException("본인의 요청만 처리할 수 있습니다.", HttpStatus.FORBIDDEN);
         }
+        log.info("아이의 암호화된 닉네임: {}", childEncryptedNickname);
 
-        String encryptedParentNickname = request.getParentEncryptedNickname();
-        log.info("부모의 암호화된 닉네임: {}", encryptedParentNickname);
+        String parentEncryptedNickname = request.getParentEncryptedNickname();
+        log.info("부모의 암호화된 닉네임: {}", parentEncryptedNickname);
         log.info("연동 요청 승인 여부: {}", dto.isApproved());
 
-        // 2. 사용자 정보 조회
-        Users childUser = userRepository.findByEncryptedNickname(encryptedChildNickname).orElseThrow(() -> new IllegalArgumentException("해당 아이 유저가 존재하지 않습니다."));
-        Users parentUser = userRepository.findByEncryptedNickname(encryptedParentNickname).orElseThrow(() -> new IllegalArgumentException("해당 부모 유저가 존재하지 않습니다."));
-
+        // 3. 사용자 정보 조회
+        Users childUser = userRepository.findByEncryptedNickname(childEncryptedNickname).orElseThrow(() -> new IllegalArgumentException("해당 아이 유저가 존재하지 않습니다."));
+        Users parentUser = userRepository.findByEncryptedNickname(parentEncryptedNickname).orElseThrow(() -> new IllegalArgumentException("해당 부모 유저가 존재하지 않습니다."));
         if (childUser == null || parentUser == null) {
             throw new CustomException("유저 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
         }
 
+        // 4. 아이의 linked_id가 이미 존재하는데 approved(승인)한 경우 -> 수락 불가(denied)
         List<String> linkedIds = childUser.getLinked_id();
         if (dto.isApproved() && linkedIds != null && !linkedIds.isEmpty()) {
             log.info("이미 다른 계정과 연동되어 있어 수락 불가");
@@ -72,31 +76,102 @@ public class AccountChildService {
             throw new CustomException("이미 다른 계정과 연동되어 있어 수락할 수 없습니다.", HttpStatus.BAD_REQUEST);
         }
 
-        // 승인하는 경우
+        // 5. 승인하는 경우
         if (dto.isApproved()) {
             request.setApproved(true);
             request.setStatus("approved");
             accountRepository.update(request);
 
-            if (!childUser.getLinked_id().contains(encryptedParentNickname)) {
-                childUser.getLinked_id().add(encryptedParentNickname);
+            // 아이와 부모의 linked_id에 각각 연동할 (암호화된) 닉네임이 없는 경우에만 추가하기
+            if (!childUser.getLinked_id().contains(parentEncryptedNickname)) {
+                childUser.getLinked_id().add(parentEncryptedNickname);
             }
-            if (!parentUser.getLinked_id().contains(encryptedChildNickname)) {
-                parentUser.getLinked_id().add(encryptedChildNickname);
+            if (!parentUser.getLinked_id().contains(childEncryptedNickname)) {
+                parentUser.getLinked_id().add(childEncryptedNickname);
             }
+
+            // 아이와 부모 유저 linked_id 업데이트
             userRepository.update(childUser);
             userRepository.update(parentUser);
 
-            log.info("부모-자녀 연결 완료: {} <-> {}", encryptedParentNickname, encryptedChildNickname);
+            log.info("부모-자녀 연결 완료: {} <-> {}", parentEncryptedNickname, childEncryptedNickname);
 
             return childUser;
         } else {
+            // 6. 거절하는 경우
             request.setApproved(false);
             request.setStatus("denied");
             accountRepository.update(request);
             log.info("연동 요청 거절 완료");
-            return null; // 거절 시 토큰 재발급 없음
+            return null;
         }
+    }
+
+    /**
+     * 아이에게 누락된 연동 요청 푸시알림 (FCM) 다시 보냄
+     */
+    public ResponseEntity<Map<String, Object>> sendAccountRequest(String childEncryptedNickname, String pendingRequestId)
+            throws ExecutionException, InterruptedException {
+
+        Map<String, Object> response = new HashMap<>();
+
+        // 자녀 유저 조회
+        Users childUser = userRepository.findByEncryptedNickname(childEncryptedNickname)
+                .orElseThrow(() -> new CustomException("해당 닉네임의 자녀를 찾을 수 없습니다.", HttpStatus.NOT_FOUND));
+
+        // 해당 요청 가져오기
+        PendingAccountRequest accountRequest = accountRepository.findByRequestId(pendingRequestId);
+        String parentEncryptedNickname = accountRequest.getParentEncryptedNickname();
+
+        // 이미 다른 부모와 연동된 자녀인지 확인
+        log.info("이미 연동된 자녀인지 체크..");
+        List<String> linkedIds = childUser.getLinked_id();
+        if (linkedIds != null && linkedIds.stream().anyMatch(Objects::nonNull)) {
+            // PendingAccountRequest denied로 업데이트
+            accountRequest.setStatus("denied");
+            accountRequest.setApproved(false);
+            accountRepository.update(accountRequest);
+
+            response.put("success", false);
+            response.put("message", "해당 자녀 계정은 이미 다른 계정과 연동되어 있습니다.");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+
+        // 연동 요청을 보낼 아이의 FcmToken 찾기
+        List<FcmToken> fcmTokens = fcmTokenService.getTokensByHashedUserId(childUser.getNicknameHash()); //아이디 해시값으로 찾기
+
+        try {
+            if(!fcmTokens.isEmpty()){
+                // 5. 아이의 존재하는 모든 FcmToken 으로 푸시알림 전송
+                for(FcmToken fcmToken : fcmTokens){
+                    log.info("아이의 fcmToken: {}", fcmToken.getToken());
+                    FcmRequestDTO requestDTO = FcmRequestDTO.builder()
+                            .user_id(fcmToken.getUser_id()) // 해시된 user_id
+                            .fcmToken(fcmToken.getToken()) // 그냥 아무것도 안된 token
+                            .notification(FcmRequestDTO.Notification.builder()
+                                    .title("iPlan")
+                                    .body(aes.decrypt(parentEncryptedNickname) + " 부모님이 연동 요청을 보냈습니다. 눌러서 확인하세요.") // 평문 id
+                                    .build())
+                            .data(FcmRequestDTO.Data.builder()
+                                    .pendingRequestId(pendingRequestId)
+                                    .sender(aes.decrypt(parentEncryptedNickname)) // 평문 id
+                                    .type("AccountLinkRequest")
+                                    .build())
+                            .build();
+                    fcmRequestService.sendPush(requestDTO);
+                    log.info("연동 요청 푸시알림을 성공적으로 보냈습니다.");
+                }
+            } else {
+                log.warn("연동 요청을 보낼 아이({})의 FcmToken 이 존재하지 않습니다.", childEncryptedNickname);
+            }
+        } catch (Exception e) {
+            throw new CustomException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        // 응답 반환
+        response.put("success", true);
+        response.put("message", "자녀에게 연동 요청이 전송되었습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
