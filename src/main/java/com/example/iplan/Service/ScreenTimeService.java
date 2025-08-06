7554f343350dee66efce7a24c3558ccccde9cb4a
정하은 <he3080@naver.com>
2025-08-06 15:08:53 +0900

    [bugfix] 스크린타임 사진 첫 업로드시, ‘path’ must be a non-empty String 오류가 뜸, 무시 이후부터는 업로드 문제 없음 → 원인불명, 알아내기가 너무 힘듬, 프론트에서 formData 보낼때의 로그는 전혀 문제가 없고, 백에서는 여러가지 디버깅을 시도했지만, 여전히 오리무중임 → 추측:  MultipartFile 내부 name 필드가 비어있을 수 있고, 그것이 곧바로 resolve에 사용되어서 오류 발생
    원인은 불명하나, null 체크 후 UUID로 파일명 대체하는 방어 코드를 짜서 보완하기로 결정

--- src/main/java/com/example/iplan/Service/ScreenTimeService.java
+++ src/main/java/com/example/iplan/Service/ScreenTimeService.java
@@ -150,7 +150,8 @@ public class ScreenTimeService {
 
                 String filename = image.getOriginalFilename();
                 if (filename == null || filename.trim().isEmpty()) {
-                    throw new CustomException("파일 이름이 비어 있거나 잘못되었습니다.", HttpStatus.BAD_REQUEST);
+                    log.info("파일 이름이 null 또는 비어 있음. UUID로 대체.");
+                    filename = UUID.randomUUID() + ".jpg";
                 }
 
                 Path tempDir = Files.createTempDirectory("upload");
@@ -225,7 +226,7 @@ public class ScreenTimeService {
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
                 e.printStackTrace();
-                throw new CustomException("파일 업로드 오류 발생", HttpStatus.INTERNAL_SERVER_ERROR);
+                throw new CustomException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }else{
             throw new CustomException("설치된 앱 목록이 존재하지 않습니다.", HttpStatus.NOT_FOUND);
